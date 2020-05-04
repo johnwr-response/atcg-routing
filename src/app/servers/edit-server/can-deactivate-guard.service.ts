6a80448526f88c80a4df58350952adb463407734
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-05-04 10:42:48 +0200

    Controlling Navigation with canDeactivate

---
+++ src/app/servers/edit-server/can-deactivate-guard.service.ts
@@ -0,0 +1,16 @@
+import {Observable} from "rxjs/Observable";
+import {ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree} from "@angular/router";
+
+export interface CanComponentDeactivate {
+  canDeactivate: () => Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree;
+}
+
+export class CanDeactivateGuard implements CanDeactivate<CanComponentDeactivate> {
+  canDeactivate(component: CanComponentDeactivate,
+                currentRoute: ActivatedRouteSnapshot,
+                currentState: RouterStateSnapshot,
+                nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return component.canDeactivate();
+  }
+
+}
